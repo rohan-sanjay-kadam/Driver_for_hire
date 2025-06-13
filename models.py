4e82e77574419001312dcd7475f5df23949b28d7
rohan-sanjay-kadam <kadamrohan441@gmail.com>
2025-06-13 11:38:38 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,12 +6,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    role = db.Column(db.String(10), nullable=False)  # 'Passenger' or 'Driver'
+    role = db.Column(db.String(10), nullable=False) 
 
 class Ride(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     pickup = db.Column(db.String(100))
     destination = db.Column(db.String(100))
-    status = db.Column(db.String(100), default='Pending')  # or 'Accepted by <Driver>'
+    status = db.Column(db.String(100), default='Pending') 
     passenger_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     driver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
